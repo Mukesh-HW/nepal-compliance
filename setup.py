408bc7bb728dc253db96f17cdc3dac314ee86c6d
Mukesh-HW <mkchaudhary4390@gmail.com>
2025-01-19 00:30:33 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
-from setuptools import setup, find_packages
-from nepal_compliance import __version__ as version
-setup(
-    name="nepal_compliance",  # The name of your app
-    version=version,          # Initial version
-    description="A custom app for Nepal compliance in ERPNext",
-    author="ERP Org",  # Replace with your name or your organization
-    author_email="support@gmail.com",  # Replace with your email
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    install_requires=install_requires
-)
+from setuptools import setup
+
+name = "nepal_compliance"
+
+setup()
